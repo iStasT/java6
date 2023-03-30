f2eb73ae414b152cd5f1d63bdfee04000f9d5606
Stas Terehin <da.da@bk.ru>
2023-03-30 16:35:04 +0300

    initialComit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,69 @@
+package ru.netology.stats;
+public class StatsService {
+    public long sum(long[] sales) {
+        long totalSale =0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale;
+    }
+
+    public long average(long[] sales) {
+        return sum(sales) / sales.length;
+    }
+
+    public int monthMaxSale(long[] sales) {
+       int monthMax = 0;
+       long saleMax = sales[0];
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= saleMax) {
+                monthMax = i;
+                saleMax = sales[i];
+
+        }
+
+        }
+
+        return monthMax + 1;
+    }
+
+    public int monthMinSale(long[] sales) {
+        int monthMin = 0;
+        long saleMin = sales[0];
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= saleMin) {
+                monthMin = i;
+                saleMin = sales[i];
+
+            }
+
+        }
+
+        return monthMin + 1;
+    }
+
+    public int monthsBelowAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < average(sales)){
+                counter++;
+            }
+
+        }
+        return counter;
+    }
+
+    public int monthsAboveAverage(long[] sales) {
+        long averageSale = average(sales);
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > averageSale) {
+                counter++;
+            }
+
+        }
+        return counter;
+    }
+}
